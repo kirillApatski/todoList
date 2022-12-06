34926a8ef27fb188117e5fbf9529ed82530789b0
Kirill <apatski94@gmail.com>
2022-12-07 00:33:40 +0300

    added todolist-task-reducer.test

---
+++ src/features/TodolistsList/todolists-tasks-reducer.test.ts
@@ -0,0 +1,26 @@
+import {addTodolistAC, TodolistDomainType, todolistsReducer} from './todolists-reducer';
+import {tasksReducer, TasksStateType} from './tasks-reducer';
+
+test('ids should be equals', () => {
+    const startTasksState: TasksStateType = {};
+    const startTodolistsState: Array<TodolistDomainType> = [];
+
+    const action = addTodolistAC({
+        todolist:{
+            id: 'any id',
+            title: 'new todolist',
+            addedDate: '',
+            order: 0
+        }
+    });
+
+    const endTasksState = tasksReducer(startTasksState, action)
+    const endTodolistsState = todolistsReducer(startTodolistsState, action)
+
+    const keys = Object.keys(endTasksState);
+    const idFromTasks = keys[0];
+    const idFromTodolists = endTodolistsState[0].id;
+
+    expect(idFromTasks).toBe(action.payload.todolist.id);
+    expect(idFromTodolists).toBe(action.payload.todolist.id);
+});
